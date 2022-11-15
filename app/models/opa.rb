28f2c9d81ff396c7955799dcc029c0c4968bd628
Yana <yana.videoeditor.gmail.com>
2022-11-15 15:00:14 -0700

    show_order_info method

--- app/models/opa.rb
+++ app/models/opa.rb
@@ -1,4 +1,12 @@
 class Opa < ActiveRecord::Base
   belongs_to :order
   belongs_to :product
+
+  def new_opa(order)
+    Order.create(order_id: order.id, product_id: product.id)
+  end
+
+  def print_details
+    "order number: #{order_id}, date: #{order.date}, product name: #{product.name}, product category: #{product.category}, price: $#{product.price}"
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,15 @@ Order.destroy_all
 Product.destroy_all
 Opa.destroy_all
 
-User.create(name: "customer1", admin?: false)
-User.create(name: "customer2", admin?: false)
-User.create(name: "customer3", admin?: false)
+u1 = User.create(name: "customer1", admin?: false)
+u2 = User.create(name: "customer2", admin?: false)
+u3 = User.create(name: "customer3", admin?: false)
 
-Product.create(name: "BRASS SPRINKLER 155 QR", description: "155 DEGREE BRASS QUICK RESPONSE SPRINKLER", category:"SPRINKLER", price: 10.10, img: "https://cdn11.bigcommerce.com/s-hn9twptgqs/images/stencil/608x608/products/762/1243/RA1425__67885.1648147329.jpg?c=1" )
-Product.create(name: "BRASS SPRINKLER 175 QR", description: "175 DEGREE BRASS QUICK RESPONSE SPRINKLER", category: "SPRINKLER", price: 10.20, img: "https://cdn11.bigcommerce.com/s-hn9twptgqs/images/stencil/608x608/products/762/1243/RA1425__67885.1648147329.jpg?c=1")
-Product.create(name: "BRASS SPRINKLER 200 QR", description: "200 DEGREE BRASS QUICK RESPONSE SPRINKLER", category: "SPRINKLER", price: 10.30, img: "https://cdn11.bigcommerce.com/s-hn9twptgqs/images/stencil/608x608/products/762/1243/RA1425__67885.1648147329.jpg?c=1")
-Product.create(name: "CHROME SPRINKLER 155 QR", description: "155 DEGREE CHROME QUICK RESPONSE SPRINKLER", category: "SPRINKLER", price: 10.40, img: "https://h4j7v6p7.stackpathcdn.com/wp-content/uploads/2022/10/F1FR56_pendent_chrome-e1666876440191-279x600.png?x77572")
-Product.create(name: "CHROME SPRINKLER 175 QR", description: "175 DEGREE CHROME QUICK RESPONSE SPRINKLER", category: "SPRINKLER", price: 10.50, img: "https://h4j7v6p7.stackpathcdn.com/wp-content/uploads/2022/10/F1FR56_pendent_chrome-e1666876440191-279x600.png?x77572")
+p1 = Product.create(name: "BRASS SPRINKLER 155 QR", description: "155 DEGREE BRASS QUICK RESPONSE SPRINKLER", category:"SPRINKLER", price: 10.10, img: "https://cdn11.bigcommerce.com/s-hn9twptgqs/images/stencil/608x608/products/762/1243/RA1425__67885.1648147329.jpg?c=1" )
+p2 = Product.create(name: "BRASS SPRINKLER 175 QR", description: "175 DEGREE BRASS QUICK RESPONSE SPRINKLER", category: "SPRINKLER", price: 10.20, img: "https://cdn11.bigcommerce.com/s-hn9twptgqs/images/stencil/608x608/products/762/1243/RA1425__67885.1648147329.jpg?c=1")
+p3 = Product.create(name: "BRASS SPRINKLER 200 QR", description: "200 DEGREE BRASS QUICK RESPONSE SPRINKLER", category: "SPRINKLER", price: 10.30, img: "https://cdn11.bigcommerce.com/s-hn9twptgqs/images/stencil/608x608/products/762/1243/RA1425__67885.1648147329.jpg?c=1")
+p4 = Product.create(name: "CHROME SPRINKLER 155 QR", description: "155 DEGREE CHROME QUICK RESPONSE SPRINKLER", category: "SPRINKLER", price: 10.40, img: "https://h4j7v6p7.stackpathcdn.com/wp-content/uploads/2022/10/F1FR56_pendent_chrome-e1666876440191-279x600.png?x77572")
+p5 = Product.create(name: "CHROME SPRINKLER 175 QR", description: "175 DEGREE CHROME QUICK RESPONSE SPRINKLER", category: "SPRINKLER", price: 10.50, img: "https://h4j7v6p7.stackpathcdn.com/wp-content/uploads/2022/10/F1FR56_pendent_chrome-e1666876440191-279x600.png?x77572")
 Product.create(name: "CHROME SPRINKLER 200 QR", description: "200 DEGREE CHROME QUICK RESPONSE SPRINKLER", category: "SPRINKLER", price: 10.60, img: "https://h4j7v6p7.stackpathcdn.com/wp-content/uploads/2022/10/F1FR56_pendent_chrome-e1666876440191-279x600.png?x77572")
 Product.create(name: "BRASS SPRINKLER 155 SR", description: "155 DEGREE BRASS STANDARD RESPONSE SPRINKLER", category: "SPRINKLER", price: 12.00, img: "https://cdn11.bigcommerce.com/s-hn9twptgqs/images/stencil/608x608/products/762/1243/RA1425__67885.1648147329.jpg?c=1")
 Product.create(name: "BRASS SPRINKLER 175 SR", description: "175 DEGREE BRASS STANDARD RESPONSE SPRINKLER", category: "SPRINKLER", price: 12.10, img: "https://cdn11.bigcommerce.com/s-hn9twptgqs/images/stencil/608x608/products/762/1243/RA1425__67885.1648147329.jpg?c=1")
@@ -32,9 +32,15 @@ Product.create(name: "3 PIPE BLK STL", description: "3\" PIPE BLACK STEEL", cate
 Product.create(name: "4 PIPE BLK STL", description: "4\" PIPE BLACK STEEL", category: "PIPE", price: 3.75, img: "https://www.plumbingsupplynow.com/pub/media/webp_image/catalog/product/cache/9d08971813a040f8f96067a40f75c615/b/l/blacknipplelong_109.webp")
 Product.create(name: "6 PIPE BLK STL", description: "6\" PIPE BLACK STEEL", category: "PIPE", price: 4.50, img: "https://www.plumbingsupplynow.com/pub/media/webp_image/catalog/product/cache/9d08971813a040f8f96067a40f75c615/b/l/blacknipplelong_109.webp")
 
-Order.create(date: "23.12.2012", total: "$25", user_id: 1)
+o1 = Order.create(date: "23.12.2012", total: "$25", user_id: 1)
+o2 = Order.create(date: "13.11.2013", total: "$28", user_id: 1)
+o3 = Order.create(date: "20.11.2005", total: "$29", user_id: 2)
 
-Opa.create(order_id: 1, product_id: 1)
-Opa.create(order_id: 1, product_id: 2)
+opa1 = Opa.create(order_id: o1.id, product_id: p1.id)
+opa2 = Opa.create(order_id: o1.id, product_id: p2.id)
+opa3 = Opa.create(order_id: o2.id, product_id: p3.id)
+opa4 = Opa.create(order_id: o3.id, product_id: p4.id)
+
+
 
 puts "Done seeding!"
