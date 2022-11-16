377b72c82903679217ab829ddeacac0b53bb1281
jbrochu1 <111677770+jbrochu1@users.noreply.github.com>
2022-11-16 11:45:12 -0700

    added yana order method

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ActiveRecord::Base
     has_many :opas
     has_many :products, through: :opas
+
+    def show_order_info
+      opas.group_by { |opa| opa.print_details }
+    end
 end
