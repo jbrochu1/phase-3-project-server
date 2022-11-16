ebe4a66a647933a0d3b8a0a5b116762fffc0fbc5
jbrochu1 <111677770+jbrochu1@users.noreply.github.com>
2022-11-16 11:45:49 -0700

    added yana product method

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ActiveRecord::Base
     has_many :opas
+
+    def fav_prod
+      return nill if opas.count == 0
+  
+      opas.group_by { |opa| opa.product_id }.max_by { |product_id| product_id.count }
+    end
 end
