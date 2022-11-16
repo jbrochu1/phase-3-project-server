f9a25ffa1c2068a6bc22b38c54abe3dd1de425b0
jbrochu1 <111677770+jbrochu1@users.noreply.github.com>
2022-11-16 11:44:10 -0700

    added yana methods to opa

--- app/models/opa.rb
+++ app/models/opa.rb
@@ -9,4 +9,12 @@ class Opa < ActiveRecord::Base
   #     description: product.description
 
   # end
+
+  def new_opa(order)
+    Order.create(order_id: order.id, product_id: product.id)
+  end
+
+  def print_details
+    "order number: #{order_id}, date: #{order.date}, product name: #{product.name}, product category: #{product.category}, price: $#{product.price}"
+  end
 end
